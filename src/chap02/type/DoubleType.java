2d1b35c7e6b6e81fa9b83e506795919ff675afa6
sebaek <sebaek@gmail.com>
2020-09-29 14:40:45 +0900

    int, double

---
+++ src/chap02/type/DoubleType.java
@@ -0,0 +1,15 @@
+package chap02.type;
+
+public class DoubleType {
+	public static void main(String[] args) {
+		// double : 8byte
+		
+		double doubleValue = 3.14;
+		System.out.println(doubleValue);
+		
+		doubleValue = 3e6; // 3.0 * 10의6제곱
+		System.out.println(doubleValue);
+		
+		
+	}
+}
