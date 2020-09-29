98edbb74a9533aa1da37d80e1ba36e8a4c3b33ae
sebaek <sebaek@gmail.com>
2020-09-29 11:40:17 +0900

    first

---
+++ src/chap02/textbook/VariableExample.java
@@ -0,0 +1,13 @@
+package chap02.textbook;
+
+public class VariableExample {
+
+	public static void main(String[] args) {
+		int value = 10;
+		
+		int result = value + 10;
+		
+		System.out.println(result);
+	}
+	
+}
