07a5280480ebbe38109ac4e27b92d41d6fd4acfa
sebaek <sebaek@gmail.com>
2020-10-05 14:32:03 +0900

    infinite nan

---
+++ src/chap02/type/StringType.java
@@ -0,0 +1,15 @@
+package chap02.type;
+
+public class StringType {
+	public static void main(String[] args) {
+		// String: 문자열 저장
+		String carName;
+		carName = "Kia";
+		
+		System.out.println(carName);
+		System.out.println("");
+		System.out.println("여러문자열..");
+		System.out.println("큰따옴표\" 작은따옴표'");
+		
+	}
+}
