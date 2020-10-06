e2b2a3f9e23b5aee6f1169445ef3e749d2f0eba0
sebaek <sebaek@gmail.com>
2020-10-06 14:18:32 +0900

    string

---
+++ src/chap11/string/StringEx1.java
@@ -0,0 +1,13 @@
+package chap11.string;
+
+public class StringEx1 {
+	public static void main(String[] args) {
+		String str1 = "java";
+		char c1 = str1.charAt(0);
+		System.out.println(c1);
+		System.out.println(str1.charAt(1));
+		System.out.println(str1.charAt(2));
+		System.out.println(str1.charAt(3));
+		System.out.println(str1.charAt(4));
+	}
+}
