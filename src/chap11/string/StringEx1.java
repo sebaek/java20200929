425570064e59c755c1b695992afcdc92d9a471c1
sebaek <sebaek@gmail.com>
2020-10-06 15:30:29 +0900

    string

--- src/chap11/string/StringEx1.java
+++ src/chap11/string/StringEx1.java
@@ -28,8 +28,45 @@ public class StringEx1 {
 		String str7 = "자바 프로그래밍 자바";
 		int a = str7.indexOf("자바로");
 		System.out.println(a);
+		
+		// length
+		String str8 = "이것이 va";
+		int l = str8.length();
+		System.out.println(l);
+		System.out.println(str8.charAt(l - 1));
+		
+		// replace
+		String str9 = "abcdefabcdefg";
+		String str10 = str9.replace("abc", "123");
+		System.out.println(str10);
+		System.out.println(str9);
+		
+		
+		// substring
+		String str11 = "0123456789gjgj6757g";
+		String sub = str11.substring(2, 3);
+		System.out.println(sub);
+		sub = str11.substring(5, str11.length());
+		System.out.println(sub);
+		
+		sub = str11.substring(6);
+		System.out.println(sub);
+		
+		// trim
+		String str12 = " ja       va ";
+		String t = str12.trim();
+		System.out.println(t);
+		System.out.println(str12);
+		
 	}
 }
+
+
+
+
+
+
+
 
 
 
