4b9ab7b1a9c434c524f2ddd783dcf2ff5876ca33
sebaek <sebaek@gmail.com>
2020-10-07 14:09:56 +0900

    read

---
+++ src/chap04/textbook/WhileKeyControlExample.java
@@ -0,0 +1,17 @@
+package chap04.textbook;
+
+public class WhileKeyControlExample {
+	public static void main(String[] args) throws Exception {
+		/*
+		System.out.println("입력>");
+		int i = System.in.read();
+		int j = System.in.read();
+		int k = System.in.read();
+		
+		System.out.println("출력");
+		System.out.println(i);
+		System.out.println(j);
+		System.out.println(k);
+		*/
+	}
+}
