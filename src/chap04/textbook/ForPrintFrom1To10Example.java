416c7aa14505a5fc7f48691104add9f213185728
sebaek <sebaek@gmail.com>
2020-10-07 11:10:51 +0900

    for

---
+++ src/chap04/textbook/ForPrintFrom1To10Example.java
@@ -0,0 +1,29 @@
+package chap04.textbook;
+
+public class ForPrintFrom1To10Example {
+	public static void main(String[] args) {
+		// 1 -> 2 -> code -> 3 -> 2 
+	
+		// 1~10
+		for (int i = 1; i <= 10; i++) {
+			System.out.println(i);
+		}
+		
+		// 11~20
+		// 1~9 홀수만
+		// 10~1
+		// 10~2 짝수만
+		
+		
+		
+	}
+}
+
+
+
+
+
+
+
+
+
