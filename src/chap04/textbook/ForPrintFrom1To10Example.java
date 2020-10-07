5845c46f051b305738341f996777649a1d60ac35
sebaek <sebaek@gmail.com>
2020-10-07 11:24:12 +0900

    for

--- src/chap04/textbook/ForPrintFrom1To10Example.java
+++ src/chap04/textbook/ForPrintFrom1To10Example.java
@@ -8,12 +8,26 @@ public class ForPrintFrom1To10Example {
 		for (int i = 1; i <= 10; i++) {
 			System.out.println(i);
 		}
-		
+		System.out.println("---------------------");
 		// 11~20
+		for (int i = 11; i <= 20; i++) {
+			System.out.println(i);
+		}
+		System.out.println("---------------------");
 		// 1~9 홀수만
+		for (int i = 1; i <= 9; i+=2) {
+			System.out.println(i);
+		}
+		System.out.println("---------------------");
 		// 10~1
+		for (int i = 10; i >= 1; i--) {
+			System.out.println(i);
+		}
+		System.out.println("---------------------");
 		// 10~2 짝수만
-		
+		for (int i = 10; i >= 2; i-=2) {
+			System.out.println(i);
+		}
 		
 		
 	}
