10c18f52b5da5eb506feac026e3ea871048fce05
sebaek <sebaek@gmail.com>
2020-10-08 16:34:34 +0900

    exercise09

--- src/chap05/exercises/Exercise09.java
+++ src/chap05/exercises/Exercise09.java
@@ -19,15 +19,39 @@ public class Exercise09 {
 			int selectNo = scanner.nextInt();
 
 			if (selectNo == 1) {
-
+				System.out.print("학생수>");
+				studentNum = scanner.nextInt();
+				scores = new int[studentNum];
 			} else if (selectNo == 2) {
-
+				
+				for (int i = 0; i < studentNum; i++) {
+					System.out.print("scores[" + i + "]>");
+					scores[i] = scanner.nextInt();
+				}
+				
 			} else if (selectNo == 3) {
-
+				for (int i = 0; i < scores.length; i++) {
+					System.out.println("scores[" + i + "]: " + scores[i]);
+				}
 			} else if (selectNo == 4) {
-
+				int sum = 0;
+				int max = Integer.MIN_VALUE;
+				double avg = 0.0;
+				
+				for (int i = 0; i < scores.length; i++) {
+					if (max <= scores[i]) {
+						max = scores[i];
+					}
+					
+					sum += scores[i];
+				}
+				
+				avg = (double) sum / scores.length;
+				
+				System.out.println("최고 점수 : " + max);
+				System.out.println("평균 점수 : " + avg);
 			} else if (selectNo == 5) {
-
+				run = false;
 			}
 		}
 
