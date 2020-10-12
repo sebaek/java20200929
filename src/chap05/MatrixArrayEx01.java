ee8cf18ee604df0cc24e362f29dd57ada3cc20d9
sebaek <sebaek@gmail.com>
2020-10-12 09:41:56 +0900

    matrix

---
+++ src/chap05/MatrixArrayEx01.java
@@ -0,0 +1,14 @@
+package chap05;
+
+public class MatrixArrayEx01 {
+	public static void main(String[] args) {
+		int[][] matrix = new int[3][2];
+		
+		int[] arr = new int[3];
+		System.out.println(arr.length);
+		
+		System.out.println(matrix[0].length);
+		System.out.println(matrix[1].length);
+		System.out.println(matrix[2].length);
+	}
+}
