ac05b47d14ce22093eabae2adb75562ebc476187
sebaek <sebaek@gmail.com>
2020-10-12 11:43:11 +0900

    for eacy

---
+++ src/chap05/exercises/Exercise0105.java
@@ -0,0 +1,12 @@
+package chap05.exercises;
+
+public class Exercise0105 {
+	public static void main(String[] args) {
+		// 1번 문제: 4
+		// 2번 문제: 3
+		// 3번 문제: 2
+		// 4번 문제: 2
+		int[] a = {1, 2, 3};
+		// 5번 문제: 3
+	}
+}
---
+++ src/chap05/exercises/Exercise08.java
@@ -0,0 +1,38 @@
+package chap05.exercises;
+
+public class Exercise08 {
+	public static void main(String[] args) {
+		int[][] array = {
+				{95, 86},
+				{83, 92, 96},
+				{78, 83, 93, 87, 88}
+		};
+		System.out.println(array.length);
+		System.out.println(array[0].length);
+		System.out.println(array[1].length);
+		System.out.println(array[2].length);
+		
+		System.out.println("-------------");
+		
+		int sum = 0;
+		double avg = 0.0;
+		int cnt = 0;
+		for (int i = 0; i < array.length; i++) {
+			for (int j = 0; j < array[i].length; j++) {
+				sum += array[i][j];
+				cnt++;
+			}
+		}
+		
+		avg = (double) sum / cnt;
+		
+		System.out.println("sum : " + sum);
+		System.out.println("avg : " + avg);
+	}
+}
+
+
+
+
+
+
