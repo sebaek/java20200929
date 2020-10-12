40fa01886214f00f5c331c071b7ba244a39627dc
sebaek <sebaek@gmail.com>
2020-10-12 10:31:11 +0900

    matrix2

--- src/chap05/MatrixArrayEx01.java
+++ src/chap05/MatrixArrayEx01.java
@@ -10,5 +10,25 @@ public class MatrixArrayEx01 {
 		System.out.println(matrix[0].length);
 		System.out.println(matrix[1].length);
 		System.out.println(matrix[2].length);
+		
+		arr[0] = 99;
+		arr[1] = 88;
+		arr[2] = 77;
+		
+		matrix[0][0] = 9;
+		matrix[2][0] = 1;
+		
+		matrix[2][2] = 2;
+//		matrix[3][0] = 3;
+		
 	}
 }
+
+
+
+
+
+
+
+
+
