8faa27d5edb3d162c00fc1a19ccf11491677dedf
sebaek <sebaek@gmail.com>
2020-10-14 09:25:15 +0900

    static

---
+++ src/chap06/lecture/staticKeyword/MyClass.java
@@ -0,0 +1,14 @@
+package chap06.lecture.staticKeyword;
+
+public class MyClass {
+	static int var2;
+	int var1;
+	
+	void method1() {
+		System.out.println(var1);
+	}
+	
+	static void method2() {
+		System.out.println(var2);
+	}
+}
---
+++ src/chap06/textbook/s061002/CalculatorExample.java
@@ -0,0 +1,5 @@
+package chap06.textbook.s061002;
+
+public class CalculatorExample {
+
+}
