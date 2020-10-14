c1a8ce54ff2e7a5bd002f7e44c43df1926bd6e11
sebaek <sebaek@gmail.com>
2020-10-14 12:33:29 +0900

    access modifier

--- src/chap06/textbook/s061303/package1/B.java
+++ src/chap06/textbook/s061303/package1/B.java
@@ -1,5 +1,15 @@
 package chap06.textbook.s061303.package1;
 
 public class B {
-
+	public B() {
+		A a = new A();
+		a.field1 = 1;
+		a.field2 = 1;
+//		a.field3 = 1;
+		
+		a.method1();
+		a.method2();
+//		a.method3();
+		
+	}
 }
