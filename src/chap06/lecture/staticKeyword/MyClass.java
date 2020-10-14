91c606eacba8e42f5a3cd3d1b625bc421d04e2e2
sebaek <sebaek@gmail.com>
2020-10-14 09:41:12 +0900

    static

--- src/chap06/lecture/staticKeyword/MyClass.java
+++ src/chap06/lecture/staticKeyword/MyClass.java
@@ -4,11 +4,22 @@ public class MyClass {
 	static int var2;
 	int var1;
 	
+	static {
+		System.out.println("static block");
+		var2 = 3000;
+	}
+	
 	void method1() {
+		method2();
+		System.out.println(var2);
 		System.out.println(var1);
 	}
 	
 	static void method2() {
+//		method1();
+//		System.out.println(var1);
 		System.out.println(var2);
 	}
 }
+
+
