9c966d7e652e1bd3a6cd0528bc4dedb20729bc23
sebaek <sebaek@gmail.com>
2020-10-15 12:25:20 +0900

    polymorphism

---
+++ src/chap07/textbook/s070701/Child.java
@@ -0,0 +1,12 @@
+package chap07.textbook.s070701;
+
+public class Child extends Parent {
+	@Override
+	public void method2() {
+		System.out.println("child-method2");
+	}
+	
+	public void method3() {
+		System.out.println("chlid-method3");
+	}
+}
