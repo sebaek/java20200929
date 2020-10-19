f1bdda05a03801e4d3673a8cfc1519994e5e36ec
sebaek <sebaek@gmail.com>
2020-10-19 09:27:05 +0900

    interface

---
+++ src/chap08/textbook/s080303/MyApp.java
@@ -0,0 +1,22 @@
+package chap08.textbook.s080303;
+
+import chap08.textbook.s080201.RemoteControl;
+
+public class MyApp {
+	public static void main(String[] args) {
+		SmartTelevision o1 = new SmartTelevision();
+		RemoteControl r1 = o1;
+		Searchable s1 = o1;
+		
+		s1.search("daum");
+//		s1.turnOn();
+//		s1.turnOff();
+		
+		r1.turnOn();
+		r1.turnOff();
+		r1.setVolume(3);
+//		r1.search("google");
+		
+		
+	}
+}
