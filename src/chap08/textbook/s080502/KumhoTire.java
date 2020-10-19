487782de01ab42c41ede90ec730f9095eaf4fc17
sebaek <sebaek@gmail.com>
2020-10-19 10:23:39 +0900

    interface

--- src/chap08/textbook/s080502/KumhoTire.java
+++ src/chap08/textbook/s080502/KumhoTire.java
@@ -1,5 +1,8 @@
 package chap08.textbook.s080502;
 
-public class KumhoTire {
-
+public class KumhoTire implements Tire {
+	@Override
+	public void roll() {
+		System.out.println("금호 타이어가 굴러갑니다.");
+	}
 }
--- src/chap08/textbook/s080502/Tire.java
+++ src/chap08/textbook/s080502/Tire.java
@@ -1,5 +1,5 @@
 package chap08.textbook.s080502;
 
 public interface Tire {
-
+	public void roll();
 }
