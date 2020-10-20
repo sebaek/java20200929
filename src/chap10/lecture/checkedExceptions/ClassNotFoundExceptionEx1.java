e956656f3e5ae67c0d65bb405863b26dd320e3ee
sebaek <sebaek@gmail.com>
2020-10-20 11:34:25 +0900

    exception try catch

--- src/chap10/lecture/checkedExceptions/ClassNotFoundExceptionEx1.java
+++ src/chap10/lecture/checkedExceptions/ClassNotFoundExceptionEx1.java
@@ -7,13 +7,18 @@ public class ClassNotFoundExceptionEx1 {
 		try {
 			System.out.println("프로그램 실행 try 1");
 			
-			Class.forName("java.lang.String2");
+			Class.forName("java.lang.String");
 			
 			System.out.println("프로그램 실행 try 2");
 		} catch (ClassNotFoundException e) {
 			System.out.println("익셉션 발생!!!!!!");
+		} finally {
+			System.out.println("무조건 실행 !!");
 		}
 		
 		System.out.println("프로그램 실행 2");
 	}
 }
+
+
+
