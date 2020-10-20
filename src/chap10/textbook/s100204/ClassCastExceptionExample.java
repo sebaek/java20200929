28c3f7f641979015ec979abae142a022a43e951c
sebaek <sebaek@gmail.com>
2020-10-20 10:45:40 +0900

    exception

---
+++ src/chap10/textbook/s100204/ClassCastExceptionExample.java
@@ -0,0 +1,32 @@
+package chap10.textbook.s100204;
+
+public class ClassCastExceptionExample {
+	public static void main(String[] args) {
+		Dog dog = new Dog();
+		changeDog(dog);
+		
+		Cat cat = new Cat();
+		changeDog(cat);
+	}
+
+	private static void changeDog(Animal animal) {
+		if (animal instanceof Dog) {
+			Dog dog = (Dog) animal;
+		}
+	}
+}
+
+class Animal {}
+class Dog extends Animal {}
+class Cat extends Animal {}
+
+
+
+
+
+
+
+
+
+
+
