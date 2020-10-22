f8ea4250a2cc658c89ec122bf7cae12b70a62182
sebaek <sebaek@gmail.com>
2020-10-22 12:01:00 +0900

    warmup2

--- src/codingbat/WarmUp2.java
+++ src/codingbat/WarmUp2.java
@@ -11,4 +11,20 @@ public class WarmUp2 {
 		return res;
 	}
 
+	public String frontTimes(String str, int n) {
+		String res = "";
+
+		String front = str;
+
+		if (str.length() > 3) {
+			front = str.substring(0, 3);
+		}
+
+		for (int i = 0; i < n; i++) {
+			res += front;
+		}
+
+		return res;
+	}
+
 }
