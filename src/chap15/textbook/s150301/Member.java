18a49f807c7b3db1bf6918465fe5d5bcb2b8a43b
sebaek <sebaek@gmail.com>
2020-10-23 15:11:16 +0900

    set

--- src/chap15/textbook/s150301/Member.java
+++ src/chap15/textbook/s150301/Member.java
@@ -8,19 +8,38 @@ public class Member {
 		this.name = name;
 		this.age = age;
 	}
-	
-	@Override
-	public boolean equals(Object obj) {
-		if (obj instanceof Member) {
-			Member member = (Member) obj;
-			return member.name.equals(name) && (member.age == age);
-		} else {
-			return false;
-		}
-	}
-	
 	@Override
 	public int hashCode() {
-		return name.hashCode() + age;
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + age;
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		return result;
 	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Member other = (Member) obj;
+		if (age != other.age)
+			return false;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		return true;
+	}
+	
+	
 }
+
+
+
+
+
+
