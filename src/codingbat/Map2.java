e3432405fdcd90c00a98a29f2c190d6961375785
sebaek <sebaek@gmail.com>
2020-10-26 15:03:48 +0900

    map

---
+++ src/codingbat/Map2.java
@@ -0,0 +1,17 @@
+package codingbat;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Map2 {
+	public Map<String, Integer> word0(String[] strings) {
+		Map<String, Integer> map = new HashMap<>();
+
+		for (String str : strings) {
+			map.put(str, 0);
+		}
+
+		return map;
+	}
+
+}
