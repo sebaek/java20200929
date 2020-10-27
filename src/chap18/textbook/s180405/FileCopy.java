ae2a495f25b12511f31ebab2d680d4d2618e10e2
sebaek <sebaek@gmail.com>
2020-10-27 12:16:57 +0900

    writer, copy

---
+++ src/chap18/textbook/s180405/FileCopy.java
@@ -0,0 +1,13 @@
+package chap18.textbook.s180405;
+
+public class FileCopy {
+	public static void main(String[] args) {
+		String src = "src/chap18/textbook/s180405/FileCopy.java";
+		String des = "file-copy.txt";
+		
+		// Reader, Writer 사용해서 파일 복사
+		
+		
+		
+	}
+}
