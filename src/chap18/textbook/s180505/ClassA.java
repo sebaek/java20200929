8b97bd1534d8389bf1d7d427a8a47517942a3dd5
sebaek <sebaek@gmail.com>
2020-10-28 10:43:56 +0900

    serial

--- src/chap18/textbook/s180505/ClassA.java
+++ src/chap18/textbook/s180505/ClassA.java
@@ -3,6 +3,8 @@ package chap18.textbook.s180505;
 import java.io.Serializable;
 
 public class ClassA implements Serializable {
+	static final long serialVersionUID = 1L;
+	
 	int field1;
 	ClassB field2 = new ClassB();
 	static int field3;
--- src/chap18/textbook/s180505/SerializableReader.java
+++ src/chap18/textbook/s180505/SerializableReader.java
@@ -9,7 +9,7 @@ public class SerializableReader {
 		ObjectInputStream ois = new ObjectInputStream(fis);
 		
 		ClassA v = (ClassA) ois.readObject();
-		System.out.println("field1: " + v.field1);
+//		System.out.println("field1: " + v.field1);
 		System.out.println("field2.field1: " + v.field2.field1);
 		System.out.println("field3: " + v.field3);
 		System.out.println("field4: " + v.field4);
