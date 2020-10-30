ecdad4760e9593ad4299cd9cea1cdb98283617f9
sebaek <sebaek@gmail.com>
2020-10-30 11:43:27 +0900

    function

---
+++ src/chap14/lecture/api/function/FunctionEx3.java
@@ -0,0 +1,11 @@
+package chap14.lecture.api.function;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class FunctionEx3 {
+	public static void main(String[] args) {
+		Map<String, Integer> map = new HashMap<>();
+		map.put
+	}
+}
