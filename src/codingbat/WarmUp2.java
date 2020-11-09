c4aabf75515bfcaada7705b7020567129759b26c
sebaek <sebaek@gmail.com>
2020-11-09 16:40:22 +0900

    warmup2

--- src/codingbat/WarmUp2.java
+++ src/codingbat/WarmUp2.java
@@ -95,4 +95,25 @@ public class WarmUp2 {
 		return 0;
 	}
 
+	public int arrayCount9(int[] nums) {
+		int cnt = 0;
+		for (int n : nums) {
+			if (n == 9) {
+				cnt++;
+			}
+		}
+
+		return cnt;
+	}
+
+	public boolean arrayFront9(int[] nums) {
+		for (int i = 0; i < nums.length && i < 4; i++) {
+			if (nums[i] == 9) {
+				return true;
+			}
+		}
+
+		return false;
+	}
+
 }
