ee729fbe107a81645ea0dcaf681f5f3a0ba324c8
sebaek <sebaek@gmail.com>
2020-11-23 12:23:35 +0900

    warmup2

--- src/codingbat/WarmUp2.java
+++ src/codingbat/WarmUp2.java
@@ -195,4 +195,16 @@ public class WarmUp2 {
 
 	}
 
+	public boolean noTriples(int[] nums) {
+		int last = nums.length - 2;
+
+		for (int i = 0; i < last; i++) {
+			if (nums[i] == nums[i + 1] && nums[i] == nums[i + 2]) {
+				return false;
+			}
+		}
+
+		return true;
+	}
+
 }
