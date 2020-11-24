255bab5cb8160b95fdd23598f1ada9ba97812c31
sebaek <sebaek@gmail.com>
2020-11-24 16:30:02 +0900

    string1

---
+++ src/codingbat/String1.java
@@ -0,0 +1,27 @@
+package codingbat;
+
+public class String1 {
+	public String helloName(String name) {
+		return "Hello " + name + "!";
+	}
+
+	public String makeAbba(String a, String b) {
+		return a + b + b + a;
+	}
+
+	public String makeTags(String tag, String word) {
+		return "<" + tag + ">" + word + "</" + tag + ">";
+	}
+
+	public String makeOutWord(String out, String word) {
+		return out.substring(0, 2) + word + out.substring(2);
+	}
+
+	public String extraEnd(String str) {
+		int len = str.length();
+		String last = str.substring(len - 2);
+
+		return last + last + last;
+	}
+
+}
